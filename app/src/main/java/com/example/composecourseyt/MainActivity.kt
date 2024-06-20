f829e9669b18fd571caf3e6bfe48abfab980e5b5
joao.azeredo <jmaz.dev@gmail.com>
2024-06-20 18:01:57 -0300

    some other compose functionalities

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -5,25 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -31,20 +26,35 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val image = painterResource(id = R.drawable.kermit)
-            val contentDescription = "Kermit is playing banjo"
-            val title = "Kermit is playing banjo"
-            ImageCard(painter = image, contentDescription = contentDescription, title = title)
+            val color = remember {
+                mutableStateOf(Color.Yellow)
+            }
+            Column(modifier = Modifier.fillMaxSize()) {
+
+            }
+            StateChange(updateColor = { color.value = it }, color = color.value)
+
         }
     }
 }
@@ -56,50 +66,145 @@ fun ImageCard(
     title: String,
     modifier: Modifier = Modifier
 ) {
-   Box(modifier = Modifier
-       .fillMaxWidth(0.5f)
-       .padding(16.dp)){
-       ElevatedCard(
-           modifier = modifier.fillMaxWidth(),
-           shape = RoundedCornerShape(15.dp),
-           elevation = CardDefaults.elevatedCardElevation(5.dp)
-       ) {
-           Box(modifier = Modifier.height(200.dp)) {
-               Image(
-                   painter = painter,
-                   contentDescription = contentDescription,
-                   contentScale = ContentScale.FillBounds,
-                   alignment = Alignment.Center
-               )
-               Box(modifier = Modifier.fillMaxSize()
-                   .background(
-                       Brush.verticalGradient(
-                        colors =
-                            listOf(Color.Transparent, Color.Black),
-                            startY = 320f
-                       )
-                   )
-               )
-               Box(
-                   modifier = Modifier
-                       .fillMaxSize()
-                       .padding(12.dp), contentAlignment = Alignment.BottomCenter
-               ) {
-                   Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-               }
-           }
-       }
-   }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(16.dp)
+    ) {
+        ElevatedCard(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(15.dp),
+            elevation = CardDefaults.elevatedCardElevation(5.dp)
+        ) {
+            Box(modifier = Modifier.height(200.dp)) {
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop,
+                    alignment = Alignment.CenterEnd
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            Brush.verticalGradient(
+                                colors =
+                                listOf(Color.Transparent, Color.Black),
+                                startY = 320f
+                            )
+                        )
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp), contentAlignment = Alignment.BottomCenter
+                ) {
+                    Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun StylingText() {
+    val fontFamily = FontFamily(
+        Font(R.font.lexend_bold, FontWeight.Bold),
+        Font(R.font.lexend_light, FontWeight.Light),
+        Font(R.font.lexend_medium, FontWeight.Medium),
+        Font(R.font.lexend_semibold, FontWeight.SemiBold),
+        Font(R.font.lexend_thin, FontWeight.Thin),
+        Font(R.font.lexend_extralight, FontWeight.ExtraLight),
+        Font(R.font.lexend_black, FontWeight.Black),
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF101010))
+            .padding(16.dp)
+
+    ) {
+        Text(
+
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 50.sp
+                    )
+                ) {
+                    append("J")
+                }
+                append("etpack ")
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 50.sp,
+                        textDecoration = (
+                                TextDecoration.Underline
+                                        + TextDecoration.LineThrough
+                                )
+
+                    )
+
+                ) {
+                    append("C")
+                }
+                append("ompose")
+            },
+            color = Color.White, fontSize = 30.sp,
+            style = TextStyle(
+                fontFamily = fontFamily,
+                fontWeight = FontWeight.Bold,
+                letterSpacing = .3.sp,
+                textAlign = TextAlign.End,
+                fontStyle = FontStyle.Italic,
+            ),
+
+
+            )
+    }
+}
+
+@Composable
+fun StateChange(updateColor: (Color) -> Unit, color: Color) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier
+            .background(Color.Red)
+            .fillMaxSize()
+            .weight(1f)
+            .clickable {
+                updateColor(
+                    Color(
+                        red = Random.nextInt(256),
+                        green = Random.nextInt(256),
+                        blue = Random.nextInt(256),
+                        alpha = 255
+                    )
+                )
+            }
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .background(Color.Yellow)
+        )
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val color = remember {
+        mutableStateOf(Color.Yellow)
+    }
     Column(modifier = Modifier.fillMaxSize()) {
 
     }
-    val image = painterResource(id = R.drawable.kermit)
-    val contentDescription = "Kermit is playing guitar"
-    val title = "Kermit is playing guitar"
-    ImageCard(painter = image, contentDescription = contentDescription, title = title)
+    StateChange(updateColor = { color.value = it }, color = color.value)
+
+
 }
